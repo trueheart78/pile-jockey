3b7edc995b18fa3b651d6a8542a76af59a795c71
Josh Mills <josh@trueheart78.com>
2020-06-09 10:34:18 -0400

    specs

--- spec/controllers/game_titles_controller_spec.rb
+++ spec/controllers/game_titles_controller_spec.rb
@@ -12,62 +12,36 @@ RSpec.describe GameTitlesController, type: :controller do
     skip('Add a hash of attributes invalid for your model')
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # GameTitlesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  before { sign_in create :user }
 
   describe 'GET #index' do
     it 'returns a success response' do
-      GameTitle.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      create :game_title
+      get :index, params: {}
       expect(response).to be_successful
     end
   end
 
   describe 'GET #show' do
     it 'returns a success response' do
-      game_title = GameTitle.create! valid_attributes
-      get :show, params: {id: game_title.to_param}, session: valid_session
+      game_title = create :game_title
+      get :show, params: {id: game_title.to_param}
       expect(response).to be_successful
     end
   end
 
   describe 'GET #new' do
     it 'returns a success response' do
-      sign_in create :user
-      get :new, params: {name: Faker::Game.title }, session: valid_session
+      get :new, params: {name: Faker::Game.title }
       expect(response).to be_successful
     end
   end
 
   describe 'GET #edit' do
     it 'returns a success response' do
-      game_title = GameTitle.create! valid_attributes
-      get :edit, params: {id: game_title.to_param}, session: valid_session
+      game_title = create :game_title
+      get :edit, params: {id: game_title.to_param}
       expect(response).to be_successful
-    end
-  end
-
-  describe 'POST #create' do
-    context 'with valid params' do
-      it 'creates a new GameTitle' do
-        expect {
-          post :create, params: {game_title: valid_attributes}, session: valid_session
-        }.to change(GameTitle, :count).by(1)
-      end
-
-      it 'redirects to the created game_title' do
-        post :create, params: {game_title: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(GameTitle.last)
-      end
-    end
-
-    context 'with invalid params' do
-      it 'returns a success response (i.e. to display the "new" template)' do
-        post :create, params: {game_title: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
     end
   end
 
@@ -78,23 +52,23 @@ RSpec.describe GameTitlesController, type: :controller do
       }
 
       it 'updates the requested game_title' do
-        game_title = GameTitle.create! valid_attributes
-        put :update, params: {id: game_title.to_param, game_title: new_attributes}, session: valid_session
+        game_title = create :game_title
+        put :update, params: {id: game_title.to_param, game_title: new_attributes}
         game_title.reload
         skip('Add assertions for updated state')
       end
 
       it 'redirects to the game_title' do
-        game_title = GameTitle.create! valid_attributes
-        put :update, params: {id: game_title.to_param, game_title: valid_attributes}, session: valid_session
+        game_title = create :game_title
+        put :update, params: {id: game_title.to_param, game_title: valid_attributes}
         expect(response).to redirect_to(game_title)
       end
     end
 
     context 'with invalid params' do
       it 'returns a success response (i.e. to display the "edit" template)' do
-        game_title = GameTitle.create! valid_attributes
-        put :update, params: {id: game_title.to_param, game_title: invalid_attributes}, session: valid_session
+        game_title = create :game_title
+        put :update, params: {id: game_title.to_param, game_title: invalid_attributes}
         expect(response).to be_successful
       end
     end
@@ -102,15 +76,15 @@ RSpec.describe GameTitlesController, type: :controller do
 
   describe 'DELETE #destroy' do
     it 'destroys the requested game_title' do
-      game_title = GameTitle.create! valid_attributes
+      game_title = create :game_title
       expect {
-        delete :destroy, params: {id: game_title.to_param}, session: valid_session
+        delete :destroy, params: {id: game_title.to_param}
       }.to change(GameTitle, :count).by(-1)
     end
 
     it 'redirects to the game_titles list' do
-      game_title = GameTitle.create! valid_attributes
-      delete :destroy, params: {id: game_title.to_param}, session: valid_session
+      game_title = create :game_title
+      delete :destroy, params: {id: game_title.to_param}
       expect(response).to redirect_to(game_titles_url)
     end
   end
