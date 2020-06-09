770635fb365018aa8f4599811e2b31c4a685f5d2
Josh Mills <josh@trueheart78.com>
2020-06-09 10:19:20 -0400

    quotes converted

--- spec/requests/game_titles_spec.rb
+++ spec/requests/game_titles_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "GameTitles", type: :request do
-  describe "GET /game_titles" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'GameTitles', type: :request do
+  describe 'GET /game_titles' do
+    it 'works! (now write some real specs)' do
       sign_in create :user
       get game_titles_path
       expect(response).to have_http_status(200)
